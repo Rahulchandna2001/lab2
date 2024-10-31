191aaf8162ee5970fe31c7e45fdd0cbcf1f1c669
Rahulchandna2001 <95170585+Rahulchandna2001@users.noreply.github.com>
2024-10-30 23:12:35 -0400

    copy files

---
+++ app.js
@@ -0,0 +1,3 @@
+// app.js
+console.log("Hello, Jenkins CI/CD!");
+
---
+++ test.js
@@ -0,0 +1,8 @@
+// test.js
+const assert = require("assert");
+describe("Sample Test", () => {
+    it("should always pass", () => {
+        assert.strictEqual(1, 1);
+    });
+});
+
